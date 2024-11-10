df3f398e6c23a0202e6f046a271eac5190a88bb4
faisal al-anezi <fa.429@hotmail.com>
2024-11-10 16:15:54 +0300

    Initial commit

--- obj/Debug/net8.0/Visitor_System.AssemblyInfo.cs
+++ obj/Debug/net8.0/Visitor_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Visitor_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fdc9daee554e4b0ac731f6c9d3b921077d36ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Visitor_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Visitor_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
